8a334b362fb76a2e0deb302d9a289620c4feb250
HollowedRino <20200865@aloe.ulima.edu.pe>
2023-11-12 15:56:21 -0500

    cuarto commit

---
+++ include/estructuras.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "raylib.h"
+
+class Mesh{
+    private:
+    public:
+        Triangulo* trianguloIni;
+        Vertice* verticeIni;
+};
+
+class Triangulo{
+    private:
+    public:
+        int vertice1;
+        int vertice2;
+        int vertice3;
+        Triangulo* siguiente;
+};
+
+class Vertice{
+    private:
+    public:
+        Vector3 pos;
+        Vertice* siguiente;
+};
