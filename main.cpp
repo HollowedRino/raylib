b76df5b8d99a4df5a3276eff653b6218b32f06d2
walter <walter@waltdiaz.com>
2023-11-12 18:14:53 -0500

    añadido notas

--- main.cpp
+++ main.cpp
@@ -6,81 +6,81 @@
 //------------------------------------------------------------------------------------
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+  // Initialization
+  //--------------------------------------------------------------------------------------
+  const int screenWidth = 800;
+  const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - 3d camera free");
+  InitWindow(screenWidth, screenHeight, "raylib [core] example - 3d camera free");
 
-    // Define the camera to look into our 3d world
-    Camera3D camera = { 0 };
-    camera.position = (Vector3){ 10.0f, 10.0f, 10.0f }; // Camera position
-    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // Camera looking at point
-    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
-    camera.fovy = 45.0f;                                // Camera field-of-view Y
-    camera.projection = CAMERA_PERSPECTIVE;             // Camera projection type
+  // Define the camera to look into our 3d world
+  Camera3D camera = { 0 };
+  camera.position = (Vector3){ 10.0f, 10.0f, 10.0f }; // Camera position
+  camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // Camera looking at point
+  camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
+  camera.fovy = 45.0f;                                // Camera field-of-view Y
+  camera.projection = CAMERA_PERSPECTIVE;             // Camera projection type
 
-    Vector3 v1 = { 0.0f, 0.0f, 0.0f };
-    Vector3 v2 = { 0.0f, 0.0f, 9.0f };
-    Vector3 v3 = { 9.0f, 0.0f, 0.0f };
-    Vector3 v4 = { 0.0f, 9.0f, 0.0f };
+  Vector3 v1 = { 0.0f, 0.0f, 0.0f };
+  Vector3 v2 = { 0.0f, 0.0f, 9.0f };
+  Vector3 v3 = { 9.0f, 0.0f, 0.0f };
+  Vector3 v4 = { 0.0f, 9.0f, 0.0f };
 
-    // std::cout << v2.x << ", " << v2.y << ", " << v2.z << std::endl;
+  // std::cout << v2.x << ", " << v2.y << ", " << v2.z << std::endl;
 
-    DisableCursor();                    // Limit cursor to relative movement inside the window
+  DisableCursor();                    // Limit cursor to relative movement inside the window
 
-    SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+  SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
+  //--------------------------------------------------------------------------------------
 
-    // Main game loop
-    while (!WindowShouldClose())        // Detect window close button or ESC key
-    {
-        // Update
-        //----------------------------------------------------------------------------------
-        UpdateCamera(&camera, CAMERA_FREE);
+  // Main game loop
+  while (!WindowShouldClose())        // Detect window close button or ESC key
+  {
+      // Update
+      //----------------------------------------------------------------------------------
+      UpdateCamera(&camera, CAMERA_FREE);
 
-        if (IsKeyDown('Z')) camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
-        //----------------------------------------------------------------------------------
+      if (IsKeyDown('Z')) camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
+      //----------------------------------------------------------------------------------
 
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
-        // x mas importante que eje z
-        // eje X va de izquirda superior a derecha inferior, Z va de izquierda inferior a derecha superior
-            ClearBackground(RAYWHITE);
+      // Draw
+      //----------------------------------------------------------------------------------
+      BeginDrawing();
+      // x mas importante que eje z
+      // eje X va de izquirda superior a derecha inferior, Z va de izquierda inferior a derecha superior
+          ClearBackground(RAYWHITE);
 
-            BeginMode3D(camera);
+          BeginMode3D(camera);
 
-                DrawTriangle3D(v1,v3,v2,BLUE);
+              DrawTriangle3D(v1,v3,v2,BLUE);
 
-                DrawLine3D(v1,v2,BLACK);  
-                DrawLine3D(v1,v3,BLACK);
-                DrawLine3D(v1,v4,BLACK);
-                DrawLine3D(v2,v3,BLACK);
-                DrawLine3D(v2,v4,BLACK);
-                DrawLine3D(v3,v4,BLACK);
+              DrawLine3D(v1,v2,BLACK);  
+              DrawLine3D(v1,v3,BLACK);
+              DrawLine3D(v1,v4,BLACK);
+              DrawLine3D(v2,v3,BLACK);
+              DrawLine3D(v2,v4,BLACK);
+              DrawLine3D(v3,v4,BLACK);
 
-                DrawGrid(20, 1.0f);
+              DrawGrid(20, 1.0f);
 
-            EndMode3D();
+          EndMode3D();
 
-            DrawRectangle( 10, 10, 320, 93, Fade(SKYBLUE, 0.5f));
-            DrawRectangleLines( 10, 10, 320, 93, BLUE);
+          DrawRectangle( 10, 10, 320, 93, Fade(SKYBLUE, 0.5f));
+          DrawRectangleLines( 10, 10, 320, 93, BLUE);
 
-            DrawText("Free camera default controls:", 20, 20, 10, BLACK);
-            DrawText("- Mouse Wheel to Zoom in-out", 40, 40, 10, DARKGRAY);
-            DrawText("- Mouse Wheel Pressed to Pan", 40, 60, 10, DARKGRAY);
-            DrawText("- Z to zoom to (0, 0, 0)", 40, 80, 10, DARKGRAY);
+          DrawText("Free camera default controls:", 20, 20, 10, BLACK);
+          DrawText("- Mouse Wheel to Zoom in-out", 40, 40, 10, DARKGRAY);
+          DrawText("- Mouse Wheel Pressed to Pan", 40, 60, 10, DARKGRAY);
+          DrawText("- Z to zoom to (0, 0, 0)", 40, 80, 10, DARKGRAY);
 
-        EndDrawing();
-        //----------------------------------------------------------------------------------
-    }
+      EndDrawing();
+      //----------------------------------------------------------------------------------
+  }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+  // De-Initialization
+  //--------------------------------------------------------------------------------------
+  CloseWindow();        // Close window and OpenGL context
+  //--------------------------------------------------------------------------------------
 
-    return 0;
+  return 0;
 }
---
+++ notas.md
@@ -0,0 +1,47 @@
+# Tareas:
+
+## Tareas basicas
+
+1. Git funcional
+
+2. AñadirTriangulo() //Sebas
+
+3. VerificarTriangulo() //Sebas
+
+4. getters y setters a las clases //walter
+
+5. constructores //walter
+
+6. Averiguar camara estática
+
+
+## Requerimentos:
+
+1. Implementar una función llamada VerificarMesh que reciba un Mesh y devuelva un booleano
+que indique si es un Mesh válido o no. 
+
+
+2. Implementar una función llamada PintarTriangulo que reciba un Mesh y pinta los bordes del
+rectángulo.
+
+
+3. Implementar una función llamada CrearRectangulo que, recibiendo su ancho, su alto y la
+posición devuelva una estructura Mesh (tal como se especifica en el marco teórico).
+
+
+4. Implementar una función llamada CrearCubo que, recibiendo su ancho, su alto, la profundidad y
+la posición central, devuelva una estructura Mesh (tal como se especifica en el marco teórico).
+
+
+5. Implementar una función llamada PintarRectangulo que reciba una estructura Mesh y pinte los
+ejes del triángulo de color blanco. Debe utilizar la función PintarTriangulo.
+
+
+6. Implementar una función llamada MoverMesh, que reciba un Mesh y una posición destino xyz.
+
+
+7. Implementar una función llamada Animar que reciba un Mesh y muestre el movimiento
+continuo horizontal del Mesh. Este movimiento debe de repetirse hasta que se apriete la tecla Q.
+
+
+8. EleminarTriangulo()
