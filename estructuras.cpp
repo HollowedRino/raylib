53ff50d69a5c122ddbc0085c4b8c06a4db4a3ba5
HollowedRino <20200865@aloe.ulima.edu.pe>
2023-11-12 19:02:50 -0500

    anandidos definiciones de constructores

--- estructuras.cpp
+++ estructuras.cpp
@@ -8,5 +8,24 @@ Mesh::Mesh(){
 }
 
 void Mesh::anadirTriangulo(Vector3 v1, Vector3 v2, Vector3 v3){
-  
+  if (verticeIni == nullptr){
+    
+  }
+}
+
+//=======================Triangulo=============================
+Triangulo::Triangulo(int v1, int v2, int v3){
+  this->vertice1 = v1;
+  this->vertice2 = v2;
+  this->vertice3 = v3;
+  this->siguiente = nullptr;
+  this->id = 0;
+}
+
+//=======================Vertice==============================
+
+Vertice::Vertice(Vector3 pos){
+  this->pos = pos;
+  this->siguiente = nullptr;
+  this->id = 0;
 }
